6cd1ca4368b3a3210da90925edbdf14186b3d6bf
svdfsdev <58512937+svdfsdev@users.noreply.github.com>
2020-03-02 00:43:25 +0300

    feat: added implementation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+
+	if (Array.isArray(array) == false || array.length == 0) {
+		
+		return 0;
+
+	} else {
+		
+		let result = array[0];
+
+		for (let i = 1; i < array.length; i++) {
+			result = Math.min(result, array[i]);
+		}
+
+		return result;
+	}
+
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+
+	if (Array.isArray(array) == false || array.length == 0) {
+		
+		return 0;
+
+	} else {
+		
+		let result = array[0];
+
+		for (let i = 1; i < array.length; i++) {
+			result = Math.max(result, array[i]);
+		}
+
+		return result;
+	}
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+
+	if (Array.isArray(array) == false || array.length == 0) {
+		
+		return 0;
+
+	} else {
+
+		return array.reduce((a, b) => a + b) / array.length;
+	
+	}
+
 }
