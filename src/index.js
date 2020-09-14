182300df34746e03b857211aa8e94cc047feae95
svdfsdev <58512937+svdfsdev@users.noreply.github.com>
2020-09-14 13:43:39 +0300

    refactor: refactor code

--- src/index.js
+++ src/index.js
@@ -1,52 +1,23 @@
-
 exports.min = function min(array) {
-
-	if (Array.isArray(array) == false || array.length == 0) {
-		
-		return 0;
-
-	} else {
-		
-		let result = array[0];
-
-		for (let i = 1; i < array.length; i++) {
-			result = Math.min(result, array[i]);
-		}
-
-		return result;
-	}
-
-}
+    return (
+      !Array.isArray(array) || !array.length
+        ? 0
+        : Math.min(...array)
+    );
+};
 
 exports.max = function max(array) {
-
-	if (Array.isArray(array) == false || array.length == 0) {
-		
-		return 0;
-
-	} else {
-		
-		let result = array[0];
-
-		for (let i = 1; i < array.length; i++) {
-			result = Math.max(result, array[i]);
-		}
-
-		return result;
-	}
-
-}
+  return (
+    !Array.isArray(array) || !array.length
+      ? 0
+      : Math.max(...array)
+  );
+};
 
 exports.avg = function avg(array) {
-
-	if (Array.isArray(array) == false || array.length == 0) {
-		
-		return 0;
-
-	} else {
-
-		return array.reduce((a, b) => a + b) / array.length;
-	
-	}
-
-}
+  return (
+    !Array.isArray(array) || !array.length
+      ? 0
+      : array.reduce((sum, el) => sum += el, 0) / array.length
+  );
+};
